2c72aa24452f0e15b47148cc8fcb03aa006b19c8
DennisPulles <dennispulles@hotmail.com>
2021-03-31 16:50:17 +0200

    Add Filmlist Class and update .gitignore
    The .gitignore now includes the .name in /.idea. 
    The domain class of FilmList is added

---
+++ tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/domain/FilmList.java
@@ -0,0 +1,24 @@
+package nl.avans.movieapp.domain;
+
+import java.util.ArrayList;
+
+public class FilmList {
+
+    private String name;
+    private String username;
+    private ArrayList<Movie> films;
+
+    public FilmList(String name, String username){
+        this.name = name;
+        this.username = username;
+    }
+
+    public FilmList(String name, String username, ArrayList<Movie> films){
+        this.name = name;
+        this.username = username;
+        
+        this.films = new ArrayList<>();
+
+        this.films = films;
+    }
+}
