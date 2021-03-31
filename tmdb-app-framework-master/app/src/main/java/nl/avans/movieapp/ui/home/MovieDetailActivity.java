3a5d5627e5d34e61a7ee5417fa3c62d7008848d0
Akalster <72736983+Akalster@users.noreply.github.com>
2021-03-31 13:54:45 +0200

    Ik heb ronde hoeken aan de images in de detailpagina gegeven.

--- tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/ui/home/MovieDetailActivity.java
+++ tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/ui/home/MovieDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
