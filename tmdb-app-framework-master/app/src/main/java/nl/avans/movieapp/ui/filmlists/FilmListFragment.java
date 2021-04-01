12306ef8d886b3681014c4c83cdbb9ba03cd802f
David Dierckx <david.dierckx@hotmail.com>
2021-04-01 17:29:24 +0200

    Added Film list fragment

---
+++ tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/ui/filmlists/FilmListFragment.java
@@ -0,0 +1,21 @@
+package nl.avans.movieapp.ui.filmlists;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import nl.avans.movieapp.R;
+
+public class FilmListFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_film_list,container,false);
+    }
+}
