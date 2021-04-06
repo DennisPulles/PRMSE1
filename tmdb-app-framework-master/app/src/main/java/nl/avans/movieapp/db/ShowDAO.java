11f511150943c963b99929f1bc622cf6d81c3f1a
Akalster <72736983+Akalster@users.noreply.github.com>
2021-04-06 12:30:06 +0200

    Broken.

---
+++ tmdb-app-framework-master/app/src/main/java/nl/avans/movieapp/db/ShowDAO.java
@@ -0,0 +1,17 @@
+package nl.avans.movieapp.db;
+
+public class ShowDAO {
+
+    public String selectAmountShows(){
+        return "SELECT COUNT(*) FROM Show";
+    }
+
+    public String selectMovieIdShows(){
+        return "SELECT FilmId FROM Show";
+    }
+
+    public String insertShow(int showId, int movieId, int roomNr, String dateTime){
+        return "INSERT INTO Show VALUES (" + showId + "," + movieId + "," + roomNr + "," + dateTime + ")";
+    }
+
+}
